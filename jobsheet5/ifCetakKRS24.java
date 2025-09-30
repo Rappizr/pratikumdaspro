df3ac1d8f8766345ab4548f7db6504d732b39b53
Rappizr <mukhammadraffi5@gmail.com>
2025-09-30 08:25:20 +0700

    Modifikasi Percobaan 1

---
+++ jobsheet5/ifCetakKRS24.java
@@ -0,0 +1,21 @@
+package jobsheet5;
+
+import java.util.Scanner;
+
+public class ifCetakKRS24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("--Cetak KRS Siakad--");
+        System.out.println("Apakah UKT Sudah Lunas? (true/false): ");
+        boolean uktlunas = sc.nextBoolean();
+
+        if (uktlunas){
+            System.out.println("Pembayaran UKT Terverifikasi");
+            System.out.println("Silahkan Cetak KRS Dan Minta TTD DPA");
+        }else
+        System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+
+        sc.close();
+    }
+}
