b5d485dad0f4159cde7105626dfa40e31a05c6a0
Rappizr <mukhammadraffi5@gmail.com>
2025-10-21 18:37:13 +0700

    modifikasi percobaan ke 2

--- jobsheet5/SiakadFor24.java
+++ jobsheet5/SiakadFor24.java
@@ -6,7 +6,9 @@ public class SiakadFor24 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        double nilai, tertinggi = 0, terendah = 100;
+        double nilai, tertinggi = 0, terendah = 100, nilaiLulus = 60.0;
+        int jumlahLulus = 0, jumlahTLulus = 0;
+
         
         for (int i = 1; i <= 10; i++){
             System.out.print("Masukkan NIlai Mahasiswa Ke-" + i + ":");
@@ -17,9 +19,17 @@ public class SiakadFor24 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai > nilaiLulus) {
+                jumlahLulus++;
+            }else {
+                jumlahTLulus++;
+            }
         }
         System.out.println("Nilai Tertinggi : " + tertinggi);
         System.out.println("Nilai Terendah : " + terendah);
+        System.out.println("\n===== Jumlah Lulus & Tidak Lulus =====");
+        System.out.println("Jumlah Lulus : " + jumlahLulus);
+        System.out.println("Jumlah Tidak Lulus : " + jumlahTLulus);
     }
     
 }
