9e8d3b1d12bb8efef660a8199d7c73793f1afa03
Rappizr <mukhammadraffi5@gmail.com>
2025-10-29 14:53:02 +0700

    percobaan 2

--- jobsheet8/Square24.java
+++ jobsheet8/Square24.java
@@ -8,9 +8,12 @@ public class Square24 {
 
         System.out.print("Masukkan nilai n = ");
         int n = sc.nextInt();
+        for (int iOuter=1; iOuter<=n; iOuter++){
         for (int i=1; i<=n; i++) {
             System.out.print("*");
         }
+        System.out.println();
+    }
     }
     
 }
