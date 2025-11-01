4f4fb2d681072ac5a816c1ad2e87b9e34252a5a9
Rappizr <mukhammadraffi5@gmail.com>
2025-11-01 16:43:54 +0700

    modifikasi percobaan 4

--- jobsheet8/NilaiKelompok24.java
+++ jobsheet8/NilaiKelompok24.java
@@ -6,20 +6,26 @@ public class NilaiKelompok24 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int i = 1;
+        int i = 1, nilai, noK = 0;
+        float totalNilai, rataNilai, tertinggi = 0;
         while (i <= 6) {
             System.out.println("Kelompok " + i);
 
-           float totalNilai = 0;
+           totalNilai = 0;
             for (int j = 1; j <= 5; j++) {
                 System.out.print("Nilai dari kelompok penilai " + j + ": ");
-               int nilai = sc.nextInt();
+               nilai = sc.nextInt();
                 totalNilai += nilai;
             }
-           float rataNilai = totalNilai/5;
+           rataNilai = totalNilai/5;
+           if (rataNilai > tertinggi) {
+            tertinggi = rataNilai;
+            noK = i;
+           }
            System.out.println("Kelompok " + i + ": nilai rata-rata = " + rataNilai);
             i++;
         }
+        System.out.println("Nilai kelompok dengan rata-rata tertinggi diraih oleh kelompok " + noK + " dengan nilai = " + tertinggi);
     }
     
 }
