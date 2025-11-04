ac3eb721a848c53bf95393c93ebf77b45a85e65d
Rappizr <mukhammadraffi5@gmail.com>
2025-11-04 22:30:48 +0700

    modifikasi percobaan 3

--- jobsheet9/ArrayRataNilai24.java
+++ jobsheet9/ArrayRataNilai24.java
@@ -6,18 +6,36 @@ public class ArrayRataNilai24 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int [] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int jumlahMhs = sc.nextInt();
 
-        for (int i = 0; i < nilaiMhs.length; i++) {
+        int [] nilaiMhs = new int[jumlahMhs];
+        double totalL = 0, totalTL = 0;
+        int jumlahL = 0, jumlahTL = 0;
+        
+        for (int i = 0; i < nilaiMhs.length; i++){
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = sc.nextInt();
-        }for (int i = 0;i < nilaiMhs.length; i++){
-            total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                totalL += nilaiMhs[i];
+                jumlahL++;
+            }else {
+                totalTL += nilaiMhs[i];
+                jumlahTL++;
+            }
         }
-        rata2 = total/nilaiMhs.length;
-        System.out.print("Rata-rata nilai = "+rata2);
+        double rata2L = 0,rata2TL = 0;
+
+        if (jumlahL > 0) {
+            rata2L = totalL/jumlahL;
+        }if (jumlahTL > 0) {
+            rata2TL = totalTL/jumlahTL;
+        }
+
+        System.out.println("===========================");
+        System.out.println("Rata-rata milai lulus = "+rata2L);
+        System.out.println("Rata-rata tidak lulus = "+rata2TL);
+
     }
     
 }
