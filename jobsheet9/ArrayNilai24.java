bdcfcca514bb0ef16085aabf1c10500610df2d84
Rappizr <mukhammadraffi5@gmail.com>
2025-11-04 11:37:21 +0700

    modifikasi percobaan 2

--- jobsheet9/ArrayNilai24.java
+++ jobsheet9/ArrayNilai24.java
@@ -7,11 +7,15 @@ public class ArrayNilai24 {
         Scanner sc = new Scanner(System.in);
         int [] nilaiAkhir = new int[10];
 
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < nilaiAkhir.length; i++){
             System.out.print("Masukkan nilai akhir ke-"+i+" : ");
             nilaiAkhir[i] = sc.nextInt();
-        }for (int i = 0; i < 10;i++){
-            System.out.println("nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+        }for (int i = 0; i < nilaiAkhir.length;i++){
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-"+i+" Lulus!");
+            }else if (nilaiAkhir[i] < 70) {
+                System.out.println("Mahasiswa ke-"+i+" Tidak Lulus!");
+            }
         }
 
     }
