740bcbcb927087f2dd07d0f612e4f7e58e92b97a
Rappizr <mukhammadraffi5@gmail.com>
2025-11-29 17:33:47 +0700

    modifikasi percobaan 2

--- jobsheet11/Kafe24.java
+++ jobsheet11/Kafe24.java
@@ -2,9 +2,20 @@ package jobsheet11;
 
 public class Kafe24 {
     public static void main(String[] args) {
-        menu();
+        Menu("Budi", true, "DISKON30");
     }
-    public static void menu (){
+    public static void Menu(String namaPelanggan, boolean isMember,String kodepromo) {
+        System.out.println("Selamat Datang, "+namaPelanggan+"!");
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+        if (kodepromo.equals("DISKON50")) {
+            System.out.println("Kode promo diterima! Anda mendapatkan diskon 50%");
+        }else if (kodepromo.equals("DISKON30")) {
+            System.out.println("Kode promo diterima! Anda mendapatkan diskon 30%");
+        }else {
+            System.out.println("Kode promo invalid");
+        }
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
@@ -15,5 +26,4 @@ public class Kafe24 {
         System.out.println("=============================");
         System.out.println("Silahkan Pilih Menu Yang Anda Inginkan.");
     }
-    
 }
